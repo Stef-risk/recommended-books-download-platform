721d230c92d505e7ed722249de27451dfe3166d0
stefrisk <stefrisk3@gmail.com>
2021-09-29 14:39:11 +0800

    Initial Commit

---
+++ src/main/java/com/stefan/bookdownload/BookDownloadApplication.java
@@ -0,0 +1,13 @@
+package com.stefan.bookdownload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookDownloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookDownloadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stefan/bookdownload/BookDownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stefan.bookdownload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookDownloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
